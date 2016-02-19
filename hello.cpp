5608c2b1a30938dc3d800008bfc64dbd69675638
kleszcz <piokleszcz@gmail.com>
2016-02-19 11:47:37 +0100

    coreNrs

--- hello.cpp
+++ hello.cpp
@@ -3,16 +3,22 @@
 
 using namespace std;
 
-void function_1(){
-	cout<<" thread function1 ";
+void function_1(string msg){
+	cout<<" thread function1 " << msg<<endl;
 }
 
 int main()
 {
-	thread t1(function_1);
-	//cout<<"hello world"<<std::endl;
-	//cout<<"fuck off!";
-	//cout<<std::endl<<"NO! You fuck off!";
-	t1.join();
+	cout<<thread::hardware_concurrency();
+	thread t1(function_1, "1");
+	thread t2(function_1, "2");
+	for(auto i=0; i<10; i++)	
+		cout<<i<<endl;
+	cout<<"hello world"<<std::endl;
+	t1.detach();
+	cout<<"fuck off!"<<endl;
+	cout<<std::endl<<"NO! You fuck off!"<<endl;
+	t2.join();
+	
 	return 3;
 }
